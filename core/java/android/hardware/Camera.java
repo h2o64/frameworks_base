8ccf88b5641fa826dea3fcb99fb0f21defa160c3
peeyushmmx <peeyushmmx@micromaxinfo.com>
2016-06-22 08:01:26 +0000

    [BUG 205] : Revert "Base: fix facelock crash when lock screen is disabled"
    This reverts commit 6183c8a3e0c87acd2496c172e607d2e1bc80d12c.
    
    This patch blocks the exception thrown by camera JNI and as
    a result was blocking exception required for the CTS. Ensure
    that these kind of patch should not be merged.
    
    Source : YUOS/YUOPENOS
    BUGID : 205
    
    Change-Id: I58ce962eb7f3e3aa27307ba580475d5318ec77b8
    Signed-off-by: Shashank Shekhar <shashank.shekhar@micromaxinfo.com>

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -241,11 +241,7 @@ public class Camera {
      * If {@link #getNumberOfCameras()} returns N, the valid id is 0 to N-1.
      */
     public static void getCameraInfo(int cameraId, CameraInfo cameraInfo) {
-        try {
-            _getCameraInfo(cameraId, cameraInfo);
-        } catch (RuntimeException e) {
-            Log.e(TAG, "Lock screen is disabled, facelock can't get camera info");
-        }
+        _getCameraInfo(cameraId, cameraInfo);
         IBinder b = ServiceManager.getService(Context.AUDIO_SERVICE);
         IAudioService audioService = IAudioService.Stub.asInterface(b);
         try {
